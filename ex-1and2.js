f150208b9afa0bc5b7340cec599b596038deb939
pongpetr <suerdragon8995@gmail.com>
2025-04-08 17:20:41 +0700

    Bp8-pang javascript-values-variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let userAddress = "54/24 Gp 5 BangKruay-Sainoi Bang KruyBang Kruai"
+let billFood = 5000
+let openRestaurant = 1500
+const name = "Karter"
+const surname = "Anderson"
+const restaurantName = "Chef's Table By Chef James" 
 //Exercise #2: Variable Reassignment
 //Start coding here
+userAddress = "(970) 878-53292400 CountyRd#Meeker, colorado(CO), 81642"
+billFood = 5000;
+openRestaurant = 2500;
+
